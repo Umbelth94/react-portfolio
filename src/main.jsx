854c68e9c9c803b94aefe9af91f61c626976581f
Travis Umbel <Umbelth94@gmail.com>
2024-04-19 16:23:30 -0500

    Added navbar and aboutme page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+//Import pages
+import Error from './pages/Error.jsx'
+import About from './pages/About.jsx'
+
+
+//Create the in-browser router
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+    {/* <App /> */}
   </React.StrictMode>,
 )
+
+
